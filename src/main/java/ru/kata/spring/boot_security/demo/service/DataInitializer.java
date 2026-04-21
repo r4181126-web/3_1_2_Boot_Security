048e1663d91bd2247bb60a62d566d926f2dff2be
r4181126-web <r4181126@gmail.com>
2026-04-22 00:50:55 +0500

    обновил DataInitializer.java, добавил оформление в http файлы

--- src/main/java/ru/kata/spring/boot_security/demo/service/DataInitializer.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/DataInitializer.java
@@ -2,11 +2,12 @@ package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.Users;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Component
@@ -15,41 +16,59 @@ public class DataInitializer implements CommandLineRunner {
     private UserService userService;
     @Autowired
     private RoleService roleService;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+
     @Override
+    @Transactional
     public void run(String... args) throws Exception {
+
         Role adminRole = roleService.findByName("ROLE_ADMIN");
         if (adminRole == null) {
             adminRole = new Role("ROLE_ADMIN");
             roleService.saveRole(adminRole);
+            System.out.println("Роль ADMIN создана");
         }
+        // Создание роли USER
         Role userRole = roleService.findByName("ROLE_USER");
         if (userRole == null) {
             userRole = new Role("ROLE_USER");
             roleService.saveRole(userRole);
+            System.out.println("Роль USER создана");
         }
+        // ПОЛУЧАЕМ РОЛИ ЗАНОВО ИЗ БАЗЫ
+        adminRole = roleService.findByName("ROLE_ADMIN");
+        userRole = roleService.findByName("ROLE_USER");
+        // Создание пользователя andrey
         if (userService.findByUsername("andrey") == null) {
             Users andrey = new Users();
             andrey.setUsername("andrey");
-            andrey.setPassword(passwordEncoder.encode("100"));
+            andrey.setPassword("100");
             andrey.setName("Андрей");
             andrey.setSurName("Рясной");
             andrey.setDepartment("СНГ");
             andrey.setSalary(150000);
-            andrey.setRoles(Set.of(adminRole, userRole));
+
+            Set<Role> roles = new HashSet<>();
+            roles.add(adminRole);
+            roles.add(userRole);
+            andrey.setRoles(roles);
+
             userService.saveUser(andrey);
             System.out.println("Пользователь andrey создан");
         }
+        // Создание пользователя vika
         if (userService.findByUsername("vika") == null) {
             Users vika = new Users();
             vika.setUsername("vika");
-            vika.setPassword(passwordEncoder.encode("100"));
+            vika.setPassword("100");
             vika.setName("Вика");
             vika.setSurName("Рясная");
             vika.setDepartment("Dree");
             vika.setSalary(0);
-            vika.setRoles(Set.of(userRole));
+
+            Set<Role> roles = new HashSet<>();
+            roles.add(userRole);
+            vika.setRoles(roles);
+
             userService.saveUser(vika);
             System.out.println("Пользователь vika создан");
         }
